eb83d47df115e88e67ffa4c60a88b9417d81192d
Aman <hellodevaman@gamil.com>
2025-03-17 02:29:51 +0530

    Common const strings for response

---
+++ pkg/massage/auth_messages.go
@@ -0,0 +1,8 @@
+package message
+
+const (
+	UnableToSendOtp       = "Unable to send otp"
+	UnableToVerifyOtp     = "Unable to verify otp"
+	UnableToCreateUser    = "Unable to create user"
+	IncorrectMobileNumber = "Incorrect mobile number"
+)
